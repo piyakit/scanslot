e0c19bcd719bf3a619949261c57eb45909b3c832
piyakit <piyakit.tho@gmail.com>
2024-05-14 01:47:26 +0700

    update box shadow

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,7 +10,7 @@ const Container: React.FC<ContainerProps> = ({ children }) => {
         border-8
         my-4
         rounded-3xl
-        shadow-[0px_2px_20px_10px_#8639c1]
+        shadow-[0px_0px_20px_10px_#8639c1]
         max-w-[1320px]
         container
         mx-[500px]
