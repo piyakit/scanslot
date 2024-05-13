413eeab5f0b9e3a303d5fad501193695c8726000
piyakit <piyakit.tho@gmail.com>
2024-05-13 22:10:47 +0700

    done random rtp

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ type TGameData = {
   color: string
   desc: string
   title: string
+  hours: number
+  minutes: number
+  percent: number
+  multiplier: number
 }
 
 const gameData: TGameData[] = [
@@ -17,6 +21,22 @@ const gameData: TGameData[] = [
     color: '#00b336',
     title: 'Fortune Rabbit',
     desc: 'Fortune Rabbit is a 3-reel (3 rows in reels 1 and 3, and 4 rows in reel 2) video slot featuring prize symbols up to 500x. The Fortune Rabbit Feature with 8 fortune spins may be randomly triggered during any spin. Moreover, there will be only Prize symbols on the reels during the Fortune Rabbit Feature.',
+    hours: Math.floor(Math.random() * 4) + 0,
+    minutes: Math.floor(Math.random() * 58) + 0,
+    percent: Math.floor(Math.random() * (97 - 5) + 5),
+    multiplier: 4686,
+  },
+  {
+    id: 2,
+    img: '/games/2/b17088577061232656893.png',
+    logo: '/games/2/c17088577061467506145.png',
+    color: '#bd0000',
+    title: 'Lucky Clover Lady',
+    desc: 'Lucky Clover Lady is a 5 by 6 symbols combination video slot featuring sticky winning symbols, Level-up symbol and Multiplier symbol. Lucky Clover symbols may appear during any spin, and each of them will award either a Level-up symbol or a Multiplier symbol. Any 8 or more matching symbols that appear on the reels will result in a winning combination! Additionally, when 3 Scatter symbols appear on the reels, 15 free spins will be triggered.',
+    hours: Math.floor(Math.random() * 4) + 0,
+    minutes: Math.floor(Math.random() * 59) + 0,
+    percent: Math.floor(Math.random() * 97) + 5,
+    multiplier: 4686,
   },
 ]
 
@@ -31,6 +51,10 @@ export default function Home() {
           color={game.color}
           title={game.title}
           desc={game.desc}
+          hours={game.hours}
+          minutes={game.minutes}
+          percent={game.percent}
+          multiplier={game.multiplier}
         />
       ))}
     </main>
--- components/GameCard/index.tsx
+++ components/GameCard/index.tsx
@@ -2,7 +2,10 @@ import Image from 'next/image'
 import style from './style.module.css'
 import { truncateText } from '@/utils/truncate'
 import { ProgressBar } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Prompt } from 'next/font/google'
+
+const prompt = Prompt({ subsets: ['latin'], weight: ['400'] })
 
 type GameCardProps = {
   img: string
@@ -10,6 +13,10 @@ type GameCardProps = {
   color: string
   desc: string
   title: string
+  hours: number
+  minutes: number
+  percent: number
+  multiplier: number
 }
 
 const GameCard: React.FC<GameCardProps> = ({
@@ -18,7 +25,17 @@ const GameCard: React.FC<GameCardProps> = ({
   color,
   desc,
   title,
+  hours,
+  minutes,
+  percent,
+  multiplier,
 }) => {
+  const percentStyle =
+    percent >= 0 && percent <= 35
+      ? 'danger'
+      : percent >= 36 && percent <= 70
+      ? 'warning'
+      : 'success'
   return (
     <div className='h-[400px] w-[325px] relative block'>
       <div
@@ -33,7 +50,7 @@ const GameCard: React.FC<GameCardProps> = ({
         className={`${style.g_bg} rounded-b-xl`}
         style={{ background: color }}
       ></div>
-      <div className={`absolute z-[5] top-[180px] px-4 backdrop-blur-[8px]`}>
+      <div className={`absolute z-[5] top-[180px] px-3 backdrop-blur-[8px] pt-2`}>
         <div className='flex items-center justify-center'>
           <Image
             src={logo}
@@ -42,16 +59,102 @@ const GameCard: React.FC<GameCardProps> = ({
             height={60}
             className='object-cover'
           />
-          <div className='pl-8 flex flex-col justify-center items-center py-2'>
-            <b className='block text-[18px] text-white leading-[20px] mt-2'>
+          <div className='flex flex-col justify-center items-center py-3 text-start'>
+            <b className='block text-[18px] text-white leading-[20px]'>
               {title}
             </b>
-            <span className='text-white text-[12px] opacity-50'>
+            <span className='text-white text-[12px] opacity-50 pl-6 w-[calc(100% - 80px)] h-[32px] overflow-hidden'>
               {truncateText(desc)}
             </span>
           </div>
         </div>
-        <ProgressBar striped animated variant="success" label={'40%'} now={40} />
+        <ProgressBar
+          striped
+          animated
+          variant={percentStyle}
+          label={`${percent}%`}
+          now={percent}
+        />
+        <div className='flex justify-between mt-2'>
+          <div className='flex flex-col'>
+            {hours === 0 && minutes <= 59 ? (
+              <span
+                className={`text-white text-[13px]  ${prompt.className} ${style.flipX}`}
+              >{`${minutes} นาที`}</span>
+            ) : (
+              <span
+                className={`text-white text-[13px] ${prompt.className}`}
+              >{`0${hours} ชั่วโมง ${minutes} นาที`}</span>
+            )}
+            <span
+              className={`text-white text-[12px] opacity-50 ${prompt.className}`}
+            >
+              สิ้นสุดเวลาในอีก
+            </span>
+          </div>
+          <div className='flex flex-col'>
+            <span
+              className={`text-white text-[13px]  ${prompt.className} ${style.flipX}`}
+            >{`${percent}%`}</span>
+            <span
+              className={`text-white text-[12px] opacity-50 ${prompt.className}`}
+            >
+              อัตราชนะ
+            </span>
+          </div>
+          <div className='flex flex-col ml-[-2px]'>
+            <span
+              className={`text-white text-[13px]  ${prompt.className} ${style.flipX}`}
+            >{`x${multiplier}`}</span>
+            <span
+              className={`text-white text-[12px] opacity-50 ${prompt.className}`}
+            >
+              อัตราคูณ
+            </span>
+          </div>
+        </div>
+        <div className='flex justify-between mt-2'>
+          <div className='flex flex-col'>
+            <Image
+              src='/games/bmm.png'
+              alt={''}
+              width={86.39}
+              height={16}
+              className='invert-[1] brightness-0'
+            />
+            <span
+              className={`text-white text-[12px] opacity-50 leading-3 mt-2 ${prompt.className}`}
+            >
+              ใบรับรอง
+            </span>
+          </div>
+          <div>
+            <Image
+              src='/games/GA.png'
+              alt={''}
+              width={80}
+              height={16}
+              className='invert-[1] brightness-0'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <div className='flex'>
+              <Image
+                src='/games/usa-flag.png'
+                alt={''}
+                width={27}
+                height={18}
+                className='mr-2'
+              />
+              <Image src='/games/th-flag.png' alt={''} width={27} height={18} />
+            </div>
+            <span
+              className={`text-white text-[12px] opacity-50 leading-3 mt-2 ${prompt.className}`}
+            >
+              ภาษา
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
