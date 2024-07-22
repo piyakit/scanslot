165e6890f2c0474dde6141e38960ca912b56101c
Piyakit Thongmankhong <piyakit.tho@gmail.com>
2024-07-23 00:06:03 +0700

    update demo

--- utils/gameCardDemo.ts
+++ utils/gameCardDemo.ts
@@ -9,7 +9,7 @@ export const GAMES_CARD_DEMO = [
   },
   {
     key: uuidv4(),
-    image: '/demo/2.png',
+    image: '/demo/1.png',
     demoUrl:
       'https://m.pg-demo.com/130/index.html?l=th&ot=ca7094186b309ee149c55c8822e7ecf2&btt=2&__refer=m.pg-redirect.net&or=static.pg-demo.com&from=https://botscanslot.com/',
   },
